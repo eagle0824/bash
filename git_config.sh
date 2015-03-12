908b73368def3ab53be93443b24e9ec1e4241888
eagle <l22k77@163.com>
2015-03-12 17:54:17 -0400

    update git_config

--- git_config.sh
+++ git_config.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+
+#set name
 git config --global user.name kai.liu
-git config --global user.email kai1.liu@archermind.com
+
+#set email
+git config --global user.email kai.liu@clinkway.com
+
+#set alias
 git config alias.co checkout
 git config alias.br branch
+
+#diff ignore ^M
+git config --global core.whitespace cr-at-eol
+
+#set git diff/status color
+git config --global color.status auto
+git config --global color.diff auto
+git config --global color.branch auto
+git config --global color.interactive auto
