3ca824c8b498acc3b1f059dbc9effaec22f9c7ff
eagle <l22k77@163.com>
2015-03-12 18:01:41 -0400

    update git_config.sh

--- git_config.sh
+++ git_config.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 #set name
+git config user.name eagle
 git config --global user.name kai.liu
 
 #set email
+git config user.email l22k77@163.com
 git config --global user.email kai.liu@clinkway.com
 
 #set default editor
 git config --global core.editor vim
 
 #set alias
-git config alias.co checkout 
-git config alias.st status
-git config alias.br "branch -a"
-git config alias.ct commit
-git config alias.df diff
+git config --global alias.co checkout
+git config --global alias.st status
+git config --global alias.br "branch -a"
+git config --global alias.cm commit
+git config --global alias.df diff
 
 #diff ignore ^M
 git config --global core.whitespace cr-at-eol
