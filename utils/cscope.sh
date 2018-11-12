1a8bd8568a588efbe412a8329c648154b27029aa
kai.liu <kai.liu@imotorinfo.com>
2018-11-12 15:41:52 +0800

    add aidl files to cscopse.sh

--- utils/cscope.sh
+++ utils/cscope.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find \( -path './out' -o -path './.repo' -o -path './tools' \) -prune -o -type f -a \( -name "*.h" -o -name "*.c" -o -name "*.java" -o -name "*.mk" -o -name "*.xml" -o -name "*.cpp" -o -name "*.sh" \) -print > cscope.files
+find \( -path './out' -o -path './.repo' -o -path './tools' \) -prune -o -type f -a \( -name "*.h" -o -name "*.c" -o -name "*.java" -o -name "*.mk" -o -name "*.xml" -o -name "*.cpp" -o -name "*.sh" -o -name "*.aidl" \) -print > cscope.files
 cscope -bq
