eea0e9a1df425d2f7b0325b33e445cef888a1002
kai.liu_pc <kai.liu@imotorinfo.com>
2018-11-12 18:31:49 +0800

    not reinstall wps and remote libreoffice if installed

--- env_init/tools/tools.sh
+++ env_init/tools/tools.sh
@@ -22,10 +22,13 @@ function down_install_apps() {
 pushd $DIR_TOOLS > /dev/null
 
 #下载wps并安装
-WPS_WEB_PATH='http://kdl.cc.ksosoft.com/wps-community/download/6757/wps-office_10.1.0.6757_amd64.deb'
-echo $WPS_WEB_PATH
+if [ -z $(which wps) ]; then
 
-down_install_apps $WPS_WEB_PATH
+    WPS_WEB_PATH='http://kdl.cc.ksosoft.com/wps-community/download/6757/wps-office_10.1.0.6757_amd64.deb'
+    echo $WPS_WEB_PATH
+
+    down_install_apps $WPS_WEB_PATH
+fi
 
 #下载wps字体文件并安装
 TTF_WEB_PATH='http://httpredir.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb'
@@ -33,3 +36,8 @@ TTF_WEB_PATH='http://httpredir.debian.org/debian/pool/contrib/m/msttcorefonts/tt
 down_install_apps $TTF_WEB_PATH
 
 popd > /dev/null
+
+#卸载LibreOffice
+if [ -n $(which libreoffice) ]; then
+    sudo apt-get remove libreoffice-common
+fi
