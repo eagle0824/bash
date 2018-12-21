e99e33a18c1e965008b0cd45ea00dcd393f23828
kai.liu_pc <kai.liu@imotorinfo.com>
2018-12-21 19:42:58 +0800

    add gradle install

--- env_init/android/android.sh
+++ env_init/android/android.sh
@@ -37,3 +37,7 @@ source $ALIAS_BASH
 
 #compile for mt2712
 sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip device-tree-compiler
+
+#安装gradle
+curl -s "https://get.sdkman.io" | bash
+sdk install gradle 5.0
