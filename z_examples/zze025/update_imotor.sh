73523049a3689efbdecad03c44269c7c55495e70
kai.liu_pc <kai.liu@imotorinfo.com>
2018-12-25 11:53:58 +0800

    update some bash

---
+++ z_examples/zze025/update_imotor.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+IMOTOR_REPOSITORIES=(
+build_scripts 
+bootable/bootloader/lk
+kernel-4.9
+device/mediatek/mt2712
+vendor/mediatek/mt2712/fastRvc
+)
+#build_scripts gitolite@192.168.1.152:imotor/tools/build_scripts
+#kernel gitolite@192.168.1.152:zze025/kernel-4.9
+
+for repository in ${IMOTOR_REPOSITORIES[@]}
+do
+    echo "update repository: $repository"
+    pushd ${PWD}/${repository}> /dev/null
+    git pull
+    popd > /dev/null
+done
