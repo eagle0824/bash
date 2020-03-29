9bce3e8047469df99213f21edd37c14ab0eeb7ac
eagle <l22k77@163.com>
2020-03-29 13:55:24 +0800

    add ANDROID_SDK_ROOT on android.sh
    Signed-off-by: eagle <l22k77@163.com>

--- env_init/android/android.sh
+++ env_init/android/android.sh
@@ -25,28 +25,30 @@ if grep -q "SDK_HOME" "$ALIAS_BASH"; then
     sed -i '/'"SDK_HOME"'/d' $ALIAS_BASH
 fi
 
+if grep -q "ANDROID_SDK_ROOT" "$ALIAS_BASH"; then
+    sed -i '/'"ANDROID_SDK_ROOT"'/d' $ALIAS_BASH
+fi
+
 sleep 1
 
 sdk_home='SDK_HOME=/work/tools/android-sdk'
 sdk_path='export PATH=$SDK_HOME/tools:$SDK_HOME/platform-tools:$PATH'
 
+#gradlew compile app used ANDROID_SDK_ROOT
+android_sdk_home='export ANDROID_SDK_ROOT=/work/tools/android-sdk'
+
 echo $sdk_home >> $ALIAS_BASH
 echo $sdk_path >> $ALIAS_BASH
-
-source $ALIAS_BASH
-
-#compile for mt2712
-sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip device-tree-compiler
+echo $android_sdk_home >> $ALIAS_BASH
 
 #安装gradle
 curl -s "https://get.sdkman.io" | bash
 source ~/.bashrc
-sdk install gradle 5.0
+sdk install gradle 5.1.1
 
 #sdk install java
-sdk install java 8.0.192-zulu
-sdk install java 7.0.201-zulu
-sdk install java 6.0.119-zulu
+sdk install java 8.0.242-zulu
+sdk install java 7.0.242-zulu
 
 #添加环境变量JAVA_HOME
 if grep -q "JAVA_HOME" "$ALIAS_BASH"; then
@@ -55,12 +57,16 @@ fi
 
 sleep 1
 
-java_home='JAVA_HOME=/home/imotor/.sdkman/candidates/java/current'
+java_home='JAVA_HOME=/home/eagle/.sdkman/candidates/java/current'
 java_path='export PATH=$JAVA_HOME/bin:$PATH'
 
 echo $java_home >> $ALIAS_BASH
 echo $java_path >> $ALIAS_BASH
 
+source $ALIAS_BASH
 
 #安装kotlin
 #sudo snap install --classic kotlin
+
+#compile for mt2712
+sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip device-tree-compiler
